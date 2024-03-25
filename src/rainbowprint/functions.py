cfd1094ad357e92506eb4e2bd2b7d8ac7156374e
TristanCantatGaudin <tristancantat@gmail.com>
2024-03-25 10:18:35 +0100

    add docstring

--- src/rainbowprint/functions.py
+++ src/rainbowprint/functions.py
@@ -1,6 +1,12 @@
 import matplotlib.pyplot as plt
 
 def rainbowprint(input_string,cmap_name='cool'):
+	"""
+	Input:	input_string	the string to print
+ 		optional cmap_name: the name of the matplotlib color map to use
+
+   	Output: None
+ 	"""
 	try: # catch incorrect map names:
 		colormap = plt.get_cmap(cmap_name)
 	except:
